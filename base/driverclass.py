bafe7c5c609e323285039c91a1bf639a78eb7b76
Ajinkya <ajinkya.shukla20@gmail.com>
2022-07-26 19:04:41 +0530

    Updated all the changes in the files

--- base/driverclass.py
+++ base/driverclass.py
@@ -10,22 +10,14 @@ class WebDriverClass:
     def getWebDriver(self, browserName):
         driver = None
         if browserName == "Chrome":
-<<<<<<< HEAD
             driver = webdriver.Chrome(ChromeDriverManager().install())
-=======
-            driver = webdriver.Chrome("C:/Users/ajin/Desktop/Selenium/drivers/chromedriver.exe")
->>>>>>> 62405b59513be86d0ad1e5e07a36ff3cc20d13dc
             self.log.info("Chrome browser is initialised")
         elif browserName == "Safari":
             driver = webdriver.Safari()
             self.log.info("Safari browser is initialised")
         elif browserName == "Firefox":
-<<<<<<< HEAD
             driver = webdriver.Firefox(GeckoDriverManager.install())
-=======
-            driver = webdriver.Firefox(
-                executable_path="C:/Users/ajin/Desktop/Selenium/drivers/geckodriver.exe")
->>>>>>> 62405b59513be86d0ad1e5e07a36ff3cc20d13dc
+
             self.log.info("Firefox browser is initialised")
 
         return driver
